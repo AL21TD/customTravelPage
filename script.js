612577056c7dbc299a72075c52ac72a8cc961074
AndreasL21 <130002712+AndreasL21@users.noreply.github.com>
2023-11-07 13:19:10 +0100

    mvp

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var showCodeButton = document.getElementById("showCodeButton");
+
+    // Event-Listener für den Button hinzufügen
+    showCodeButton.addEventListener("click", function () {
+        var selectedRegion = document.getElementById("countries").value;
+
+        fetch('/db/codes.csv')
+
+            .then(response => response.text())
+            .then(csvData => {
+                var lines = csvData.split("\n");
+                var regions = lines[0].split(";");
+                var rows = lines.slice(1);
+
+                var regionIndex = regions.indexOf(selectedRegion);
+                if (regionIndex !== -1) {
+                    var codes = rows.map(row => row.split(";")[regionIndex]);
+                    var nextCode = codes.find(code => code.trim() !== '');
+                    if (nextCode) {
+                        localStorage.setItem("generatedCode", nextCode);
+                        localStorage.setItem("selectedRegion", selectedRegion);
+                        window.location.href = "code.html";
+                    } else {
+                        alert("Kein Code verfügbar für die ausgewählte Region.");
+                    }
+                } else {
+                    alert("Ungültige Region ausgewählt.");
+                }
+            })
+            .catch(error => {
+                console.error('Fehler beim Laden der CSV-Datei:', error);
+            });
+    });
+});
